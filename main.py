1307b0cb633d9b024fe1e7e628b4bce02b3d4590
zxbeyblade70-a11y <zxbeyblade70@gmail.com>
2026-01-30 14:10:43 +0700

    Add function to get document from GitHub
    Implement function to fetch document from GitHub and handle errors.

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+
+def get_document_from_github(target_url):
+    try:
+        #1. requests to github
+        response = requests.get(target_url)
+        
+        #2. check status (200 is ok)
+        if response.status_code == 200:
+            # setting to support thailand language
+            response.encoding = 'utf-8'
+            
+            #3. content to text
+            content = response.text
+            return content
+        else:
+            return f"X not found file. (Error Code: {response.status_code})"
+    except Exception as e: # ย้ายมาให้ตรงกับ try
+        return f"Error not connect to server: {e}"
+	   	    	
+# test program
+# ex : pull text from github
+url = "https://raw.githubusercontent.com/python/cpython/main/LICENSE"
+document_text = get_document_from_github(url)
+
+print("starting to read document")
+print(document_text)
+print("ending to read document")
